268a24462722a533bbfe14163e1bd7db92a58103
fussy113 <s1220080@u-aizu.ac.jp>
2016-10-26 14:27:56 +0900

    回転軸の修正、その他

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,7 @@ import javafx.scene.shape.MeshView;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
-import java.awt.AWTException;
-import java.awt.Rectangle;
-import java.awt.Robot;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -22,36 +20,59 @@ import javax.imageio.ImageIO;
 public class Main extends Application {
 
     private static final String stl = "mono.stl";
-    static int count = 0;
-    static int n = 0;
+    private static int count = 0;
+    private static int n = 0;
     private AnimationTimer timer;
     private final Rotate cameraRotateX = new Rotate(0, Rotate.X_AXIS);
     private final Rotate cameraRotateY = new Rotate(0, Rotate.Y_AXIS);
     private final Rotate cameraRotateZ = new Rotate(0, Rotate.Z_AXIS);
-    private final Translate cameraTranslate = new Translate(0, -45 , -350);
-    private final Rotate stlRotateX = new Rotate(15, Rotate.X_AXIS);
-    private final Rotate stlRotateY = new Rotate(30, Rotate.Y_AXIS);
+    private final Translate cameraTranslate = new Translate(0, 0 , -260);
+    private final Rotate stlRotateX = new Rotate(0, Rotate.X_AXIS);
+    private final Rotate stlRotateY = new Rotate(0, Rotate.Y_AXIS);
     private final Rotate stlRotateZ = new Rotate(0, Rotate.Z_AXIS);
-    private final Translate stlTranslate = new Translate(0, 0 ,0);
+    private final Translate stlTranslate = new Translate(-50, 75 ,0);
     private static Scene scene;
     private static Group root;
     private static AmbientLight ambientLight;
     private static PointLight pointLight;
     private static PerspectiveCamera cam;
-    private static final Color lightColor = Color.rgb(244, 255, 250);
-    private static final Color stlColor = Color.WHITE;
-
+    private static final Color stlColor = Color.WHITESMOKE;
+    private static final String[] filename = stl.split("\\.");
+    private static final File rmfile = new File("./" + filename[0] + "/" + filename[0] + n + ".png");
 
     //stl読み込み
     static MeshView[] loadMeshViews() {
-
         File file = new File(stl);
         StlMeshImporter importer = new StlMeshImporter();
         importer.read(file);
         Mesh mesh = importer.getImport();
-
         return new MeshView[] { new MeshView(mesh) };
     }
+
+    //スクショ保存用のディレクトリの作成、シーンの作成
+    private Group buildScene() {
+        File newdir = new File(filename[0]);
+        newdir.mkdir();
+        MeshView[] meshViews = loadMeshViews();
+        for (int i = 0; i < meshViews.length; i++) {
+            PhongMaterial stlMaterial = new PhongMaterial(stlColor);
+            stlMaterial.setSpecularPower(3);
+            meshViews[i].setMaterial(stlMaterial);
+            meshViews[i].getTransforms().addAll(stlTranslate,stlRotateX,stlRotateY,stlRotateZ);
+        }
+
+        //回転座標の更新
+        stlRotateX.setPivotY(-85);
+        stlRotateX.setPivotZ(-25);
+        stlRotateY.setPivotX(55);
+        stlRotateY.setPivotZ(-31);
+        stlRotateZ.setPivotX(55);
+        stlRotateZ.setPivotY(-85);
+        root = new Group(meshViews);
+
+        return root;
+    }
+
 
     //スクリーンショット
     public static void screenshot(){
@@ -63,28 +84,17 @@ public class Main extends Application {
         }
         BufferedImage image = robot.createScreenCapture(new Rectangle(240, 25, 800, 800));
         try {
-            ImageIO.write(image, "PNG", new File("./mono/mono" + n++ + ".png"));
+            ImageIO.write(image, "PNG", new File("./" + filename[0] + "/" + filename[0] + n + ".png"));
+
+            //レンダリングが最初の方間に合わず真っ黒画面がスクリーンショットされるのでrmします。
+            if(count<5){
+                rmfile.delete();
+                return;
+            }
+            n++;
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    //スクショ保存用のディレクトリの作成、シーンの作成
-    private Group buildScene() {
-        File newdir = new File("mono");
-        newdir.mkdir();
-
-        MeshView[] meshViews = loadMeshViews();
-        for (int i = 0; i < meshViews.length; i++) {
-            PhongMaterial stlMaterial = new PhongMaterial(stlColor);
-            stlMaterial.setSpecularColor(lightColor);
-            stlMaterial.setSpecularPower(16);
-            meshViews[i].setMaterial(stlMaterial);
-            meshViews[i].getTransforms().setAll(stlTranslate,stlRotateX,stlRotateY,stlRotateZ);
-        }
-        root = new Group(meshViews);
-
-        return root;
     }
 
     @Override
@@ -119,35 +129,22 @@ public class Main extends Application {
         };
         timer.start();
 
-
-
     }
-
 
     public void update(long now){
-        //最初の読み込み部分のスクリーンショットをしないために飛ばしています。
-        if (count<4) {
-            count++;
-            return;
-        }
-        if (count==103) System.exit(0);
+
+        if (count==505) System.exit(0);
         count++;
-        stlRotateX.setAngle(stlRotateX.getAngle()+3);
-        stlRotateY.setAngle(stlRotateY.getAngle()+5);
-        stlRotateZ.setAngle(stlRotateZ.getAngle()+6);
-        if (stlRotateX.getAngle()==360.0) stlRotateX.setAngle(0);
-        if (stlRotateY.getAngle()==360.0) stlRotateY.setAngle(0);
-        if (stlRotateZ.getAngle()==360.0) stlRotateZ.setAngle(0);
-        try {
-            Thread.sleep(30);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //回転
+        stlRotateX.setAngle(stlRotateX.getAngle()+2);
+        stlRotateY.setAngle(stlRotateY.getAngle()+3);
+        stlRotateZ.setAngle(stlRotateZ.getAngle()+0.5);
+        if (stlRotateX.getAngle()>360.0) stlRotateX.setAngle(stlRotateX.getAngle()-360);
+        if (stlRotateY.getAngle()>360.0) stlRotateY.setAngle(stlRotateY.getAngle()-360);
+        if (stlRotateZ.getAngle()>360.0) stlRotateZ.setAngle(stlRotateZ.getAngle()-360);
         screenshot();
+
     }
-
-
-
 
     public static void main(String[] args) {
         launch(args);
